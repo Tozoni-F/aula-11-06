862aab2c08f9cd9416dc6b21fa32b1334841ab38
Tozoni-F <fabiotozoni@gmail.com>
2021-06-16 20:26:45 -0300

    Segundo Teste

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("ver")
+const Product = {
+    type: "Laptop",
+    Manufacturer: "Dell",
+    Year: 2021,
+    model: "Latitude 5470",
+    Price: 9000,
+};
+var morning_discount= 0.1;
+var afternoon_discount= 0.08;
+let period = ()=>{
+    
+    if (new Date().getHours < 12){
+        return true
+        }
+        else return false
+}
+
+console.log(period())
+

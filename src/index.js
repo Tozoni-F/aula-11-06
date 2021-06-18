c5cb396a8d769af3c704669900d4c28e2a1f57a7
Tozoni-F <fabiotozoni@gmail.com>
2021-06-18 11:27:33 -0300

    Tudo separado, e classe funcionando

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const discount = require("./discount");
-const product = require('./product');
+const Product = require("./product");
 
-// var hour = new Date().getHours();
 
 // final_price(computer,discount(period(hour)));
-let smarthphone = new product("Smarthphone","Android","Samsung",2021,"S21",6250)
+const pc = new Product("Smarthphone","Android","Samsung",2021,"S21",6250,.2);
 // final_price(smarthphone,discount(period(hour)));
 // console.log(print(computer))
 // console.log(print(smarthphone))
-console.log(smarthphone)
+console.log(pc.name)
--- src/product.js
+++ src/product.js
@@ -1,17 +1,14 @@
 //Cria o objeto produto com suas caracteristicas
-//final price tem que ser definido depois, pois depende da hora
-function produtc (clas,type,manufacturer,year,model,price,){
-    this.clas=clas,
-    this.type = type,
-    this.manufacturer = manufacturer,
-    this.year = year,
-    this.model = model,
-    this.price = price,
-    this.final_price = 
-};
-function final_price(obj,discount){
-    obj.final_price = obj.price-obj.price*discount;
-};
-
-module.exports = produtc;
-module.exports = final_price;
+class Product {
+    constructor(name, type, manufacturer, year, model, price, discount) {
+        this.name = name;
+        this.type = type;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.model = model;
+        this.price = price;
+        this.discount=discount;
+        this.final_price= this.price - this.price*this.discount;
+        };
+}
+module.exports = Product

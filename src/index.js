87b20a0c07629abc8a63f72c788de6aa988f899d
Tozoni-F <fabiotozoni@gmail.com>
2021-06-18 11:51:57 -0300

    Final, objetivo atingido, criar um produto que tem o desconto atribuido conforme os periodos do dia

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 const discount = require("./discount");
 const print = require("./print");
 const Product = require("./product");
-
-
-// final_price(computer,discount(period(hour)));
-const cel = new Product("Smarthphone","Android","Samsung",2021,"S21",6250,.2);
-// final_price(smarthphone,discount(period(hour)));
-// console.log(print(computer))
-// console.log(print(smarthphone))
+const cel = new Product("Smarthphone","Android","Samsung",2021,"S21",6250,discount);
 const imprime = print(cel);
 console.log(imprime)
